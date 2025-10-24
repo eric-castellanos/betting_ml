6ed89ed28af9304a9af74c1e2a433c0b53e6a724
eric-castellanos <ericcastellanos229@gmail.com>
2025-10-23 23:14:00 -0400

    add logger and enhance fetch function

--- data-service/fetch_data.py
+++ data-service/fetch_data.py
@@ -2,11 +2,16 @@
 
 import requests
 import os
+import logging
 from typing import Dict, List, Any
 
 import polars as pl
 
-def fetch_play_by_play_data(urls: List[str]) -> Dict[str, pl.DataFrame]:
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levlname)s - Line Number %(lineno)d")
+
+logger = logging.getLogger(__name__)
+
+def fetch_play_by_play_data(years: List[str], urls: List[str]) -> Dict[str, pl.DataFrame]:
     """
 	Fetch NFL data from the given API URL.
 	Args:
@@ -16,27 +21,33 @@ def fetch_play_by_play_data(urls: List[str]) -> Dict[str, pl.DataFrame]:
 	"""
     data_dict = {}
 
-    for index, url in enumerate(urls):
-        data_dict[str(index)] = url
+    for year, url in zip(years, urls):
+        try:
+            data_dict[f"play_by_play_{year}"] = pl.read_parquet(url)
+        except Exception as e:
+            logger.error(f"Could not read in parquet file from {url} for {year}: {e}")
+        else:
+            logger.debug(f"Successfully read in play by play data for {year} from: {url}")
 
     return data_dict
 
-def save_data(data: Dict, filename: str) -> None:
+def save_data(data: pl.DataFrame, filename: str, local: bool = False) -> None:
 	"""
 	Save data to a local JSON file.
 	Args:
 		data (dict): Data to save.
 		filename (str): Path to the output file.
+		local (bool): Bool to determine whether or not to save locally.
 	"""
-	# TODO: Implement saving logic
-	pass
+	if local:
+		data.write_parquet(filename)
 
 def main():
 	# TODO: Set your API URL and parameters
-	api_url = "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds?regions=us&markets=totals,spreads,h2h&oddsFormat=american&apiKey=YOUR_API_KEY"  # Replace with actual NFL data API
-	params = {}
-	data = fetch_nfl_data(api_url, params)
-	save_data(data, "nfl_data.json")
+	years = ["2020", "2021", "2022", "2023", "2024"]
+	urls = [f"https://github.com/nflverse/nflverse-data/releases/download/pbp/play_by_play_{year}.parquet" for year in years]
+	data = fetch_play_by_play_data(years, urls)
+	#save_data(data, "nfl_data.json")
 
 if __name__ == "__main__":
 	main()
