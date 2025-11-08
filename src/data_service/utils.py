bae83467cd85686fcc3da4c87f914855d5b4ddeb
eric-castellanos <ericcastellanos229@gmail.com>
2025-11-08 18:00:37 -0500

    update files, forgot to commit

--- src/data_service/utils.py
+++ src/data_service/utils.py
@@ -38,20 +38,22 @@ def save_data(data: pl.DataFrame, bucket : str, key : str, filename: Optional[st
             logger.info(f"Writing {filename} to local folder: {local_path}")
             data.write_parquet(full_path)
 
-    try:
-        s3_client.head_object(Bucket=bucket, Key=s3_key)
-        logger.info(f"Skipping S3 upload: s3://{bucket}/{s3_key} already exists.")
-        return  # File exists, skip upload
-    except ClientError as e:
-        if e.response["Error"]["Code"] == "404":
-            # Object not found — proceed to upload
-            pass
-        else:
-            logger.error(f"Error checking S3 for {s3_key}: {e}")
-            raise
+    
+    if not local:
+        try:
+            s3_client.head_object(Bucket=bucket, Key=s3_key)
+            logger.info(f"Skipping S3 upload: s3://{bucket}/{s3_key} already exists.")
+            return  # File exists, skip upload
+        except ClientError as e:
+            if e.response["Error"]["Code"] == "404":
+                # Object not found — proceed to upload
+                pass
+            else:
+                logger.error(f"Error checking S3 for {s3_key}: {e}")
+                raise
 
-    buffer = io.BytesIO()
-    data.write_parquet(buffer)
-    buffer.seek(0)
-    logger.info(f"Uploading new file to s3://{bucket}/{s3_key}")
-    s3_client.upload_fileobj(buffer, bucket, s3_key)
+        buffer = io.BytesIO()
+        data.write_parquet(buffer)
+        buffer.seek(0)
+        logger.info(f"Uploading new file to s3://{bucket}/{s3_key}")
+        s3_client.upload_fileobj(buffer, bucket, s3_key)
